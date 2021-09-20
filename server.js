de4040959ea4e39b9f2ce0385601d469ea009d8c
Bossuyt Reinout <bossuyre@cp105386.gentgrp.gent.be>
2021-09-20 14:46:14 +0200

    update

--- server.js
+++ server.js
@@ -15,19 +15,31 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
+app.get('/en', (req, res) => {
+  res.sendFile(__dirname + '/index_en.html')
+})
+
 app.post('/feedback', (req, res) => {
   var timestamp = new Date().getTime();
   req.body.timestamp = timestamp;
   console.log(req.body)
   fs.writeFile('./feedback/'+timestamp+'.json', JSON.stringify(req.body), function (err) {
     if (err) return console.log(err);
-    res.redirect('/bedankt')
+    if(req.body.lang == "nl") {
+      res.redirect('/bedankt')
+    } else {
+      res.redirect('/thanks')
+    }
   });
 
 })
 
 app.get('/bedankt', (req, res) => {
   res.sendFile(__dirname + '/bedankt.html')
+})
+
+app.get('/thanks', (req, res) => {
+  res.sendFile(__dirname + '/thanks.html')
 })
 
 app.listen(3000, function() {
